a8cbc9aa4bd37cfb298c518e5c34e2195c79eefc
jwf12 <jwf_12@hotmail.com>
2023-02-27 19:41:27 -0300

    Se modifica modales

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,10 @@
 from django.forms import ModelForm
 from .models import Tareas
+from django import forms
 
 
 class TareasForm(ModelForm):
+   
     class Meta:
         model = Tareas
         fields = [
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Tareas
 from .forms import TareasForm
+from django.urls import reverse
 
 class Home(generic.TemplateView):
     template_name = 'home.html'
@@ -14,11 +15,14 @@ class Home(generic.TemplateView):
 
     
 class TareasView(generic.CreateView):
-    template_name = 'todo.html'
+    model = Tareas
+    template_name = 'home.html'
     form_class = TareasForm
     success_url = 'home.html'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-
+    
+    def get_success_url(self):
+        return reverse('todo:home')
